402b3110f49c760ac3ba5bf0ffe539fac5ce336f
roobottom <roo@roodesign.co.uk>
2022-04-22 14:33:59 +0100

    content updates

--- source/data/navigation.js
+++ source/data/navigation.js
@@ -12,17 +12,17 @@ module.exports = {
   ],
   footer: [
     {
-      url: '/work/',
-      title: 'Work',
-      id: 'work'
-    }, {
       url: '/articles/',
       title: 'Articles',
       id: 'articles'
     }, {
-      url: '/contact/',
-      title: 'Contact',
-      id: 'contact'
+      url: '/diary/',
+      title: 'Diary',
+      id: 'diary'
+    }, {
+      url: '/subjects/',
+      title: 'Subjects',
+      id: 'subjects'
     }
   ]
 }
