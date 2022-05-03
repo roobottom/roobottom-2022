65bd2e272517f3ae61fcb2f1b470329bddf748b3
roobottom <roo@roodesign.co.uk>
2022-05-03 17:35:24 +0100

    Opengraph

--- source/articles/articles.11tydata.js
+++ source/articles/articles.11tydata.js
@@ -1,5 +1,7 @@
 const slugify = require('../../lib/filters/slugify.js')
 const date = require('../../lib/filters/date.js')
+const transforms = require('../../lib/shortcodes/transforms.js')
+const firstSentence = require('../../lib/filters/first-sentence.js')
 
 module.exports = {
   layout: 'article.njk',
@@ -11,5 +13,19 @@ module.exports = {
   }, {
     title: 'Articles',
     url: '/articles/'
-  }]
+  }],
+  eleventyComputed: {
+    og: data => {
+      return {
+        type: 'article',
+        url: process.env.SITE_URL + data.page.url,
+        description: data.summary === undefined ? firstSentence(data.title) : data.summary,
+        image: {
+          url: data.cover === undefined ? `https://ik.imagekit.io/roobottom/tr:ot-${encodeURIComponent(data.title.substring(0, 26))},otc-FFFFFF,otw-1280,ots-180,otf-Bely.ttf/og-backgrond.png` : transforms(process.env.SITE_URL + data.cover, 'og'),
+          alt: data.coverAlt
+        },
+        date: date(data.date, 'YYYY-MM-DD[T]hh:mm:ss.SSS[Z]')
+      }
+    }
+  }
 }
--- source/diary/diary.11tydata.js
+++ source/diary/diary.11tydata.js
@@ -1,19 +1,32 @@
 const smartypants = require('smartypants')
 const date = require('../../lib/filters/date.js')
+const transforms = require('../../lib/shortcodes/transforms.js')
 
 module.exports = {
   layout: 'diary-item.njk',
   section_id: 'diary',
   breadcrumbs: [{
-      title: 'Home',
-      url: '/'
-    }, {
-      title: 'Diary',
-      url: '/diary/'
-    }],
+    title: 'Home',
+    url: '/'
+  }, {
+    title: 'Diary',
+    url: '/diary/'
+  }],
   eleventyComputed: {
     title: data => { 
       return data.summary === undefined || data.summary === '' ? `Diary entry for ${date(data.date)}` : smartypants.smartypants(data.summary, 1)
+    },
+    og: data => {
+      return {
+        type: 'article',
+        url: process.env.SITE_URL + data.page.url,
+        description: `A diary entry I posted on ${date(data.date, 'dddd D MMMM YYYY [at] h:mm a')}`,
+        image: {
+          url: data.photo === undefined ? `https://ik.imagekit.io/roobottom/tr:ot-${encodeURIComponent(data.title.substring(0, 26))},otc-FFFFFF,otw-1280,ots-180,otf-Bely.ttf/og-backgrond.png` : transforms(process.env.SITE_URL + data.photo[0].url, 'og'),
+          alt: data.photo === undefined ? `The words “${data.title.substring(0, 26)}” on a orange background` : data.photo[0].alt
+        },
+        date: date(data.date, 'YYYY-MM-DD[T]hh:mm:ss.SSS[Z]')
+      }
     }
   }
 }
