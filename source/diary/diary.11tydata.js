88b33441b095e757d907de3b08ceb78b842b4cf8
roobottom <roo@roodesign.co.uk>
2022-05-06 20:26:00 +0100

    updated og images font

--- source/diary/diary.11tydata.js
+++ source/diary/diary.11tydata.js
@@ -22,7 +22,7 @@ module.exports = {
         url: process.env.SITE_URL + data.page.url,
         description: `A diary entry I posted on ${date(data.date, 'dddd D MMMM YYYY [at] h:mm a')}`,
         image: {
-          url: data.photo === undefined ? `https://ik.imagekit.io/roobottom/tr:ot-${encodeURIComponent(data.title.substring(0, 26))},otc-FFFFFF,otw-1280,ots-180,otf-Bely.ttf/og-backgrond.png` : transforms(data.photo[0].url, 'og'),
+          url: data.photo === undefined ? `https://ik.imagekit.io/roobottom/tr:ot-${encodeURIComponent(data.title.substring(0, 32))},otc-FFFFFF,otw-1280,ots-140,otf-og-font.ttf/og-backgrond.png` : transforms(data.photo[0].url, 'og'),
           alt: data.photo === undefined ? `The words “${data.title.substring(0, 26)}” on a orange background` : data.photo[0].alt
         },
         date: date(data.date, 'YYYY-MM-DD[T]hh:mm:ss.SSS[Z]')
